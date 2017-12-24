a7d9b2387c9bbb273ff8bda9069f3f92bad7b75b
Katy <ihatetoast@gmail.com>
2017-12-24 02:12:35 -0600

    added editStore template and form mixin

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -8,3 +8,12 @@ exports.homePage = (req, res) => {
 
 // routes file tells us what the urls we hit are. then we shell it off to a separate file to do the actual work
 
+exports.addStore = (req, res)=> {
+  // res.send('it works!')
+  //make an edit template because it's the same
+  res.render('editStore', {title: 'Add Store'})
+
+}
+exports.createStore = (req, res) => {
+  res.json(req.body);
+}
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ const storeController = require('../controllers/storeController');
 // Do work here
 router.get('/', storeController.homePage);
 //myMiddleware calls next(); so it passes to the next fcn
-
+router.get('/add', storeController.addStore);
+//for when we post a form we need to post from add
+router.post('/add', storeController.createStore);
 
 module.exports = router;
