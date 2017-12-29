a7c6a39c830b7b3d41702dd0dc896e30c254782c
Katy <ihatetoast@gmail.com>
2017-12-29 11:25:52 -0600

    working on reset password.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const passport = require('passport');
+const crypto = require('crypto');
 
 
 //need to send passport data and tell us if we should be logged in or not: a strategy. intefaces to check if you're allowed to be logged in. we are using local strategy
@@ -24,4 +25,34 @@ exports.isLoggedIn = (req, res, next) => {
   }
   req.flash('error', "Oh noes. You must be logged in.")
   res.redirect('/login');
+}
+
+//resetting password with passport. keep!
+//$gt = greater than
+exports.reset = async (req, res) => {
+  const user = await User.findOne({
+    resetPasswordToken: req.params.token,
+    resetPasswordExpires: { $gt: Date.now()}
+  })
+  if(!user){
+    req.flash('error', "Password reset is invalid or expired.")
+    return res.redirect('/login')
+  }
+  // res.render('reset', { title: 'Reset your password.'})
+}
+
+exports.forgot = async (req, res) => {
+  //see if user with that email exists
+  const user = await User.findOne({ email: req.body.email})
+  if(!user){
+    req.flash('error', "A password reset has been mailed to you.")
+    res.redirect('/login')
+    //this white lie is better than announcing that no user exists. 
+  }
+  //set resest tokens and expiry on their acct
+  user.resetPasswordToken = crypto.randomBytes(20).toString('hex');
+  user.resetPasswordExpires = Date.now() + 3600000;
+  //send email with token
+
+  //redirect to login page
 }
